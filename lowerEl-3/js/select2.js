98a94c096dc00c2c6f83f0266d5a254a55515bb6
이경민 <ksan3940@gmail.com>
2025-09-27 13:50:37 +0900

    add timer

--- lowerEl-3/js/select2.js
+++ lowerEl-3/js/select2.js
@@ -4,6 +4,10 @@ $(function () {
   const $bgmTutorial = $("#bgm-tutorial")[0];
   const $bgmMain = $("#bgm-main")[0];
   const $selectMain = $(".select-main-2");
+  const $progressBarFill = $(".progress-bar-fill");
+
+  let timerStarted = false;
+  let gameTimer = null;
 
   $info1.on("click", function () {
     $info1.hide();
@@ -25,6 +29,8 @@ $(function () {
     $bgmMain.play();
     $selectMain.removeClass("display-none");
     $selectMain.removeClass("pointer-none");
+
+    // 게임 시작과 함께 타이머 시작
   });
 
   $(".popup-close-no").on("click", function () {
@@ -33,5 +39,56 @@ $(function () {
 
   $selectMain.on("click", function () {
     $bgmMain.pause();
+    $bgmMain.currentTime = 0;
+    startTimer();
   });
+
+  function startTimer() {
+    if (timerStarted) return;
+
+    timerStarted = true;
+
+    // progress-bar-fill 애니메이션 시작
+    $progressBarFill.addClass("start-timer");
+
+    // 30초 후 타이머 완료 함수 호출
+    gameTimer = setTimeout(() => {
+      onTimerComplete();
+    }, 30000); // 30초
+
+    console.log("30초 타이머 시작");
+  }
+
+  function onTimerComplete() {
+    console.log("30초 타이머 완료!");
+
+    // 여기에 30초 지났을 때 실행할 로직 추가
+    // 예: 게임 종료, 결과 화면 표시 등
+
+    timerStarted = false;
+    gameTimer = null;
+
+    // 필요한 경우 추가 처리
+    handleTimeUp();
+  }
+
+  function handleTimeUp() {
+    // 30초가 지났을 때의 처리 로직
+    console.log("시간 종료 처리");
+
+    // 예시: 게임 종료 처리
+    $selectMain.addClass("pointer-none");
+
+    // 음악 정지
+    if ($bgmMain) {
+      $bgmMain.pause();
+    }
+
+    // 결과 화면으로 이동하거나 다른 처리
+    // alert("시간이 종료되었습니다!");
+  }
+
+  function startGame() {
+    // 기존 게임 시작 로직
+  }
 });
