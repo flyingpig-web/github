0b5354e967de83b2ab914388fee144558a190ac5
이경민 <ksan3940@gmail.com>
2025-11-23 16:36:41 +0900

    초저3 - 활동2 악기들 인터랙션 추가

--- lowerEl-3/js/select3.js
+++ lowerEl-3/js/select3.js
@@ -14,6 +14,62 @@ $(function () {
   const $ktmWrapper = $(".ktm-wrapper");
   const $ch315 = $("#ch3-15")[0];
 
+  function bakToggle() {
+    $select3Bak.toggleClass("display-none");
+    $select3BakActive.toggleClass("display-none");
+  }
+  function bakInteraction() {
+    setTimeout(() => bakToggle(), 2500);
+    setTimeout(() => bakToggle(), 3000);
+    setTimeout(() => bakToggle(), 3500);
+    setTimeout(() => bakToggle(), 4000);
+    setTimeout(() => bakToggle(), 4400);
+    setTimeout(() => bakToggle(), 4900);
+  }
+
+  function moktuiInteraction() {
+    // 2.9초에 첫 번째 애니메이션
+    setTimeout(() => {
+      $select3Moktui.removeClass("moktui-rotate");
+      setTimeout(() => $select3Moktui.addClass("moktui-rotate"), 10);
+    }, 2900);
+
+    // 3.9초에 두 번째 애니메이션
+    setTimeout(() => {
+      $select3Moktui.removeClass("moktui-rotate");
+      setTimeout(() => $select3Moktui.addClass("moktui-rotate"), 10);
+    }, 3800);
+
+    // 4.9초에 세 번째 애니메이션
+    setTimeout(() => {
+      $select3Moktui.removeClass("moktui-rotate");
+      setTimeout(() => $select3Moktui.addClass("moktui-rotate"), 10);
+    }, 4700);
+
+    // 5.9초에 애니메이션 클래스 제거
+    setTimeout(() => {
+      $select3Moktui.removeClass("moktui-rotate");
+    }, 5900);
+  }
+
+  function chaeInteraction() {
+    setTimeout(() => {
+      $select3Chae.toggleClass("chae-play");
+    }, 3000);
+    setTimeout(() => {
+      $select3Chae.toggleClass("chae-play");
+    }, 4200);
+    setTimeout(() => {
+      $select3Chae.toggleClass("chae-play");
+    }, 4210);
+    setTimeout(() => {
+      $select3Chae.toggleClass("chae-play");
+    }, 5400);
+    setTimeout(() => {
+      $select3Chae.toggleClass("chae-play");
+    }, 5410);
+  }
+
   $select3Player.on("click", function () {
     $select3Player.addClass("display-none");
     $select3Cursor.addClass("display-none");
@@ -23,9 +79,16 @@ $(function () {
     $mainBgm.play();
     $select3Moktui.removeClass("display-none");
     $select3NoteWrapper.removeClass("display-none");
-    $select3Bak.addClass("display-none");
-    $select3BakActive.removeClass("display-none");
     $select3Chae.removeClass("display-none");
+
+    // bak interaction
+    bakInteraction();
+
+    // moktui interaction
+    moktuiInteraction();
+
+    // chae interaction
+    chaeInteraction();
 
     setTimeout(() => {
       $select3Success.removeClass("display-none").fadeIn(500);
